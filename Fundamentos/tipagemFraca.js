e13dabc810a0cc3184a7d0ab5ee29204bcd1e5ef
Genisson Gomes <genissongomes@gmail.com>
2020-12-15 16:45:01 -0300

    arquivos das 1 ao 21

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nome genéricos e siglas
+let numero = 1
+let valor = ''
+let pqp = false //Produto quimico perigoso... kkk
+
